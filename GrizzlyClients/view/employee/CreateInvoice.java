7ae2738b0ac946ce266b9e7108adaf31d38ff968
Lexy <102265882+cherymochi@users.noreply.github.com>
2023-11-17 21:54:06 -0500

    Update CreateInvoice.java

--- GrizzlyClients/view/employee/CreateInvoice.java
+++ GrizzlyClients/view/employee/CreateInvoice.java
@@ -11,7 +11,7 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
-import com.view.menu.EmployeeMenu;
+import com.view.menu.EmployeeMainMenu;
 
 import javax.swing.SpringLayout;
 import java.awt.Color;
